785fbff1bd5fc2224fd6aafc1c96ab872ab638a8
david <calhoun5321@gmail.com>
2021-05-19 15:46:30 -0700

    adds sessions controller, route, and action

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,17 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
-  # RECIPE
+  # RECIPES
   get "/recipes" => "recipes#index"
   get "/recipes/:id" => "recipes#show"
   post "/recipes" => "recipes#create"
   patch "/recipes/:id" => "recipes#update"
   delete "/recipes/:id" => "recipes#destroy"
 
-  # USER
+  # USERS
   post "/users" => "users#create"
+
+  # SESSIONS
+  post "/sessions" => "sessions#create"
+
 end
