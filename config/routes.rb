e6e97a567a2147a6340237362d103483d93531dd
david <calhoun5321@gmail.com>
2021-05-19 15:41:30 -0700

    adds user create route and action

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
+  # RECIPE
   get "/recipes" => "recipes#index"
   get "/recipes/:id" => "recipes#show"
   post "/recipes" => "recipes#create"
   patch "/recipes/:id" => "recipes#update"
   delete "/recipes/:id" => "recipes#destroy"
-  
+
+  # USER
+  post "/users" => "users#create"
 end
